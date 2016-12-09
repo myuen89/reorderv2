14391db535a4f6bec04d16b068b4a215c02ffd15
mhills <mhills@bccrc.ca>
2016-12-09 14:10:13 -0800

    added third test and vignette

---
+++ R/reorderv2-package.r
@@ -0,0 +1,9 @@
+#' reorderv2, reorder in descending order
+#'
+#' Graphing factors tend to be messy, since they are usually misaligned.
+#' Reorder is a function used to order factors in ascending order.
+#' Reorderv2 is a package which will order factors in descending order.
+#'
+#' @name reorderv2
+#' @docType package
+NULL
--- tests/testthat/test_reorder_desc.R
+++ tests/testthat/test_reorder_desc.R
@@ -3,7 +3,9 @@ context("Reording factors - descending")
 test_that("reorder_desc reorders factor levels in descending order", {
   x <- factor(c('leopard', 'kilo', 'gryphon', 'foxtrot'))
   y <- factor(c('foxtrot', 'gryphon', 'kilo', 'leopard'))
+  z <- c("hello", "blue", "cake", "wall")
 
   expect_identical(levels(reorder_desc(x)), levels(y))
   expect_identical(levels(reorder_desc(y)), as.character(x))
+  expect_message(reorder_desc(z), regexp = "Error. Input is not of class factors, it is of class ", class(d))
 })
