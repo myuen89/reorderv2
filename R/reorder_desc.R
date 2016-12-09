567c908d3d38b3096d4108f3db4c0218a208371e
mhills <mhills@bccrc.ca>
2016-12-09 13:00:53 -0800

    Edits in DESCRIPTION, NAMESPACE files. Added LICENSE file and man/reorder_desc.Rd documentation

--- R/reorder_desc.R
+++ R/reorder_desc.R
@@ -1,5 +1,18 @@
 d <- factor(c("hello", "blue", "cake", "wall"))
 
+#' Reorder factor levels in descending order
+#'
+#' Reorder an exisiting factor levels in descending order, where the new factor levels are arranged in opposite orientation to the character readout
+#'
+#' @param d factor
+#'
+#'
+#' @return factor
+#' @export
+#' @examples
+#' #' d <- factor(c("hello", "blue", "cake", "wall"))
+#' #' reorder_desc(d)
+
 reorder_desc <- function(d){
   aesc_d <- reorder(d, c(1:length(d)))
   desc_d <- dplyr::desc(sort(attr(aesc_d, "scores")))
